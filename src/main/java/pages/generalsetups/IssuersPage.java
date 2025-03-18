c4e37d33ce3fabf7600402fe7d039c45e9817613
georgekibe <georgekibe500@gmail.com>
2025-03-18 20:59:32 +0300

    Issuer E2E testing implemented

--- src/main/java/pages/generalsetups/IssuersPage.java
+++ src/main/java/pages/generalsetups/IssuersPage.java
@@ -2,13 +2,15 @@ package pages.generalsetups;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 
 public class IssuersPage {
 
     public WebDriver webDriver;
 
-    public IssuersPage(WebDriver webDriver)
-    {
+    public IssuersPage(WebDriver webDriver) {
         this.webDriver = webDriver;
     }
 
@@ -18,25 +20,109 @@ public class IssuersPage {
     private final By addingIssuer = By.xpath("//span[normalize-space()='Add Issuer']");
     private final By issuerName = By.xpath("//*[contains(@id, '_Name')]");
     private final By issuerTicker = By.xpath("//*[contains(@id, '_Code')]");
-    private final By issuerSector = By.xpath("//div[@class='dx-show-invalid-badge dx-lookup dx-lookup-popover-mode dx-textbox dx-texteditor dx-editor-outlined dx-widget dx-lookup-empty dx-dropdowneditor dx-validator dx-visibility-change-handler dx-state-hover dx-state-focused']//div[@role='combobox'][normalize-space()='Select...']");
-    private final By issuerCountry = By.xpath("//div[@class='dx-show-invalid-badge dx-lookup dx-lookup-popover-mode dx-textbox dx-texteditor dx-editor-outlined dx-widget dx-lookup-empty dx-dropdowneditor dx-state-focused dx-state-hover']//div[@role='combobox'][normalize-space()='Select...']");
+    private final By issuerSector = By.xpath("/html[1]/body[1]/div[4]/div[2]/div[1]/mat-dialog-container[1]/app-data-form[1]/div[1]/mat-dialog-content[1]/dx-form[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]");
+    private final By sectorSelection = By.xpath("//div[contains(text(),'Commercial Banks')]");
+    private final By issuerCountry = By.xpath("/html[1]/body[1]/div[4]/div[2]/div[1]/mat-dialog-container[1]/app-data-form[1]/div[1]/mat-dialog-content[1]/dx-form[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]");
+    private final By countryChoice = By.xpath("//div[contains(text(),'Algeria')]");
+    private final By saveButton = By.xpath("//span[normalize-space()='save']");
+
+    // pending Approval
+    private final By pendingApproval = By.cssSelector("body > app-root:nth-child(1) > div:nth-child(2) > div:nth-child(3) > app-issuers:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(2) > a:nth-child(1) > span:nth-child(2)");
+    private final By selectPendingApprovalRecord = By.xpath("//td[normalize-space()='Test Issuer']");
+    private final By approveButton = By.xpath("//span[normalize-space()='Approve']");
 
 
+    // update
+
+    private final By searchBox = By.xpath("/html[1]/body[1]/app-root[1]/div[1]/div[1]/app-issuers[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/app-data-grid[1]/div[1]/dx-data-grid[1]/div[1]/div[4]/div[1]/div[1]/div[3]/div[3]/div[1]/div[1]/div[1]/div[1]/input[1]");
+    private final By selectUpdateEntry = By.xpath("//td[normalize-space()='TI']");
+    private final By selectEntry = By.xpath("//td[normalize-space()='TIT']");
+    private final By updateButton = By.xpath("//span[normalize-space()='update']");
+    private final By activeIssuerTab = By.xpath("/html[1]/body[1]/app-root[1]/div[1]/div[1]/app-issuers[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/ul[1]/li[1]/a[1]/span[2]");
 
 
+    // Delete
+    private final By deleteButton = By.xpath("//span[normalize-space()='delete']");
+    private final By confirmDeletion = By.xpath("//div[@aria-label='Yes']//div[@class='dx-button-content']");
 
 
-    public void createIssuer()
-    {
+    public void createIssuer() {
         webDriver.findElement(generalSetup).click();
         webDriver.findElement(issuersSetup).click();
         webDriver.findElement(addingIssuer).click();
         webDriver.findElement(issuerName).sendKeys("Test Issuer");
         webDriver.findElement(issuerTicker).sendKeys("TI");
 
+       /* WebElement element = webDriver.findElement(issuerSector);
+        Select selectDropdown = new Select(element);
+        selectDropdown.selectByVisibleText("Commercial Banks");*/
+
+        webDriver.findElement(issuerSector).click();
+        webDriver.findElement(sectorSelection).click();
+        webDriver.findElement(issuerCountry).click();
+        webDriver.findElement(countryChoice).click();
+        webDriver.findElement(saveButton).click();
+
     }
 
+    public void approvePendingIssuer() {
+        webDriver.findElement(pendingApproval).click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        WebElement webElement = webDriver.findElement(selectPendingApprovalRecord);
+        Actions actions1 = new Actions(webDriver);
+        actions1.doubleClick(webElement).perform();
+        webDriver.findElement(approveButton).click();
 
+    }
+
+    public void updateIssuer() {
+        webDriver.findElement(activeIssuerTab).click();
+        webDriver.findElement(searchBox).sendKeys("test");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        WebElement theElement = webDriver.findElement(selectUpdateEntry);
+        Actions actions1 = new Actions(webDriver);
+        actions1.doubleClick(theElement).perform();
+        webDriver.findElement(issuerTicker).clear();
+        webDriver.findElement(issuerTicker).sendKeys("TIT");
+        webDriver.findElement(updateButton).click();
+
+    }
+
+    public void confirmIssuerUpdate() {
+        webDriver.findElement(activeIssuerTab).click();
+        webDriver.findElement(searchBox).sendKeys("test");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        WebElement theElement = webDriver.findElement(selectEntry);
+        Actions actions1 = new Actions(webDriver);
+        actions1.doubleClick(theElement).perform();
+        String actualValue = webDriver.findElement(issuerTicker).getAttribute("value");
+        String expectedValue = "TIT";
+        Assert.assertEquals(actualValue, expectedValue);
+
+    }
+
+    public void deleteTestIssuer() {
+        webDriver.findElement(deleteButton).click();
+        webDriver.findElement(confirmDeletion).click();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 
 }
--- src/test/java/tests/generalsetups/IssuersTests.java
+++ src/test/java/tests/generalsetups/IssuersTests.java
@@ -1,4 +1,42 @@
 package tests.generalsetups;
 
-public class IssuersTests {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.generalsetups.IssuersPage;
+import tests.BaseTest;
+
+public class IssuersTests extends BaseTest {
+
+    public IssuersPage issuersPage;
+    private static final Logger logger = LoggerFactory.getLogger(IssuersTests.class);
+
+    @BeforeTest
+    public void initializeDriver() {
+        issuersPage = new IssuersPage(webDriver);
+
+    }
+
+
+    @Test
+    public void creatingIssuer() {
+        logger.info("Creating Issuer started................");
+        issuersPage.createIssuer();
+        logger.info("Issue successfully created  !!!");
+        logger.info("Approving created Issue started.........");
+        issuersPage.approvePendingIssuer();
+        logger.info("Created Issuer has been successfully approved");
+        logger.info("Update Issuer process started ..............");
+        issuersPage.updateIssuer();
+        issuersPage.approvePendingIssuer();
+        issuersPage.confirmIssuerUpdate();
+        logger.info("Issuer Update process works successfully   !!!");
+        logger.info("Deletion process for Issuer initiated.............");
+        issuersPage.deleteTestIssuer();
+        logger.info("Issuer successfully deleted  !!!");
+
+    }
+
+
 }
